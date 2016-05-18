e94205bb729748d42b409a0a861a14384100ba84
Tino Fotiou <tino.fotiou@gmail.com>
2016-05-18 12:44:42 +0300

    Update panel.js

--- examples/panel.js
+++ examples/panel.js
@@ -10,7 +10,7 @@ google.maps.event.addDomListener(window, 'load', function() {
   var data = new MedicareDataSource;
 
   var view = new storeLocator.View(map, data, {
-    geolocation: false,
+    geolocation: true,
     features: data.getFeatures()
   });
 
